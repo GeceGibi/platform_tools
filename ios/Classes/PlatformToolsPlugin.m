3c001c0ce0da69d01af4dec4a5280273d4fb0434
Ömer Güven <omr.gven@gmail.com>
2022-04-11 14:36:45 +0300

    update pods

--- ios/Classes/PlatformToolsPlugin.m
+++ ios/Classes/PlatformToolsPlugin.m
@@ -1,7 +1,7 @@
 #import "PlatformToolsPlugin.h"
 
-#if __has_include(<platformtools/platformtools-Swift.h>)
-#import <platform_tools/platformtools-Swift.h>
+#if __has_include(<platform_tools/platform_tools-Swift.h>)
+#import <platform_tools/platform_tools-Swift.h>
 #else
 #import "platform_tools-Swift.h"
 #endif
